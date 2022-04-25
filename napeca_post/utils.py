0d5c24026f7a090a627ffe22422b57c3b33879be
zhounapeuw <zhecharleszhou@gmail.com>
2022-04-24 20:48:35 -0700

    added relevant files and added dynamic code to load projection images

--- napeca_post/utils.py
+++ napeca_post/utils.py
@@ -384,3 +384,19 @@ def df_to_dict(fpath):
     for condition in event_frames_df['event'].unique():
         event_frames_dict[condition] = list(event_frames_df[event_frames_df['event'] == condition]['time'])
     return event_frames_dict
+
+
+def uint8_arr(arr):
+
+    # convert data to appropriate type
+    # https://stackoverflow.com/questions/25485886/how-to-convert-a-16-bit-to-an-8-bit-image-in-opencv
+    if np.issubdtype(np.uint16, arr.dtype) | np.issubdtype(np.int16, arr.dtype) | np.issubdtype(np.float32, arr.dtype) | np.issubdtype(np.float64, arr.dtype):
+
+        data_out = arr / float(np.max(arr))  # normalize the data to 0 - 1; need to use float to ensure data can have decimals
+        data_out = 255 * data_out  # Now scale by 255
+
+    else:
+
+        data_out = arr
+
+    return data_out.astype('uint8')
